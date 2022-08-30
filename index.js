c19192178b35431078079d1039ce813c9f32094b
kai-353 <70913300+kai-353@users.noreply.github.com>
2022-08-30 13:37:54 +0200

    .

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const colors = require("colors");
+const path = require("path");
+const axios = require("axios");
+const fs = require("fs");
+
+const app = express();
+
+const log = (message) => {
+  var datetime = new Date();
+
+  fs.writeFile(
+    "./test.txt",
+    `[${datetime.toISOString()}] ${message} \n`,
+    { flag: "a+" },
+    (err) => {}
+  );
+};
+
+app.get("/dnd/:toggle", (req, res) => {
+  const { toggle } = req.params;
+  if (toggle !== "0" && toggle !== "1") {
+    res.status(400).json({ message: "invalid dnd status" });
+    return;
+  }
+  axios
+    .get(
+      `https://autoprov.gntel.nl/presence/dnd/${toggle}/f48eabf507d9f660a1d5c221ad43c45fea26c166/6310001301/?user=$active_user`
+    )
+    .then((res) => {
+      log(`${res.config.url} ${res.data} 301`);
+    }); //301
+  axios
+    .get(
+      `https://autoprov.gntel.nl/presence/dnd/${toggle}/85c087af125a59a8e3cda144ca05c94c561b3615/6310001302/?user=$active_user`
+    )
+    .then((res) => {
+      log(`${res.config.url} ${res.data} 302`);
+    }); //302
+  res.status(200).json({ message: "success" });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, console.log(`Server started on ${PORT}`.underline.bold.blue));
